5d7827880e61c11478be4144735ad4dec2dc231e
Francis Kim <franciskim722@gmail.com>
2018-06-15 23:50:59 -0700

    + gsap, interaction row 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
-
+import React, { PureComponent } from 'react';
+import {TweenLite} from 'gsap';
 import logo from './logo.svg';
 import utils from './utils';
 
-class App extends Component {
+class App extends PureComponent {
   constructor(){
     super();
 
@@ -11,15 +11,21 @@ class App extends Component {
       loaded: false,
     };
 
+    this.loadingEl = null;
+    this.logoContainer = null;
     this.routeToContent = this.routeToContent.bind(this);
   }
 
   componentDidMount(){
     const { loaded } = this.state;
-    utils.loading(loaded)
-    .then(updatedState => {
-      this.setState({loaded: updatedState});
-    });
+    console.log(window.outerHeight);
+    console.log(window.outerWidth);
+    TweenLite.from(this.loadingEl, 5, {y: window.outerHeight} );
+
+    // utils.loading(loaded)
+    // .then(updatedState => {
+    //   this.setState({loaded: updatedState});
+    // });
   }
 
   routeToContent() {
@@ -27,12 +33,21 @@ class App extends Component {
   }
 
   render() {
-    console.log(this.state);
+    console.log(TweenLite);
     return (
       <div className="app">
-        <div className="zero-logo-container" onClick={this.routeToContent}>
+        <div 
+          className="zero-logo-container" 
+          onClick={this.routeToContent}
+          ref={el => this.logoContainer = el}
+          tabIndex={1}
+        >
           <img src={logo} className="zero-logo" alt="logo" />
         </div>
+        <div
+          className="zero-loading"
+          ref={el => this.loadingEl = el}
+        />
       </div>
     );
   }
